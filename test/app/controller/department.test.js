de11a7fedc2aed46058ae736239462d60d5024ba
17Going <17701023509@163.com>
2018-06-14 23:29:26 +0800

    添加数据源用于测试

--- test/app/controller/department.test.js
+++ test/app/controller/department.test.js
@@ -9,37 +9,36 @@ const { app, assert } = require('egg-mock/bootstrap');
 
 describe('test/app/controller/department.test.js', () => {
 
-    /**
-     * 创建公司
-     */
-    it('创建公司', ()=>{
-        app.mockCsrf();
-        return app.httpRequest()
-            .post('/api/department/create')
-            .send({
-                name: '成都四方伟业软件股份有限公司'
-            })
-            .expect(200, {
-                code: 0,
-                data: []
-            });
-    });
-    /**
-     * 部门创建
-     */
-    it('部门创建', () => {
-        app.mockCsrf();
-        return app.httpRequest()
-            .post('/api/department/add')
-            .send({
-                name: '销售部',
-                parentId: 16
-            })
-            .expect(200, {
-                code: 0,
-                data: []
-            });
-    });
-
+  /**
+   * 创建公司
+   */
+  it('创建公司', () => {
+    app.mockCsrf();
+    return app.httpRequest()
+      .post('/api/department/create')
+      .send({
+        name: '成都四方伟业软件股份有限公司'
+      })
+      .expect(200, {
+        code: 0,
+        data: []
+      });
+  });
+  /**
+   * 部门创建
+   */
+  it('部门创建', () => {
+    app.mockCsrf();
+    return app.httpRequest()
+      .post('/api/department/add')
+      .send({
+        name: '销售部',
+        parentId: 1
+      })
+      .expect(200, {
+        code: 0,
+        data: []
+      });
+  });
 
 });
